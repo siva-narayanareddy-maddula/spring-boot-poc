33f0d5f0acf206a57c8723c4e0972cca51942609
Siva Narayana Reddy Maddula <siva_narayanareddy_maddula@apple.com>
2015-12-17 17:53:37 +0530

    Initial commit

---
+++ src/main/java/org/example/ws/web/api/GreetingController.java
@@ -0,0 +1,112 @@
+package org.example.ws.web.api;
+
+import java.math.BigInteger;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.example.ws.model.Greeting;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	private static BigInteger nextId;
+	private static Map<BigInteger, Greeting> greetingMap;
+	
+	private static Greeting save(Greeting greeting) {
+		if (greetingMap == null) {
+			greetingMap = new HashMap<BigInteger, Greeting>();
+			nextId = BigInteger.ONE;
+		}
+		
+		// If Update scenario..
+		BigInteger id = greeting.getId();
+		if(id != null) {
+			Greeting oldGreeting = greetingMap.get(id);
+			if(oldGreeting == null) {
+				return null;
+			}
+			greetingMap.remove(id);
+			greetingMap.put(id, greeting);
+			return greeting;
+		}
+
+		// If Create scenario..
+		greeting.setId(nextId);
+		nextId = nextId.add(BigInteger.ONE);
+		greetingMap.put(greeting.getId(), greeting);
+		return greeting;
+	}
+
+	static {
+		Greeting g1 = new Greeting();
+		g1.setText("Hello World");
+		save(g1);
+		
+		Greeting g2 = new Greeting();
+		g2.setText("Hello India");
+		save(g2);
+	}
+	
+	private static boolean delete(BigInteger id) {
+		 return greetingMap.remove(id) != null;
+	}
+	
+
+	@RequestMapping(value="/api/greetings", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Collection<Greeting>> getGreetings() {
+		Collection<Greeting> greetings = greetingMap.values();
+		return new ResponseEntity<Collection<Greeting>>(greetings, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/api/greetings/{id}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> getGreeting(@PathVariable("id") BigInteger id) {
+		
+		Greeting greeting = greetingMap.get(id);
+		if(greeting == null) {
+			return new ResponseEntity<Greeting>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Greeting>(greeting, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/api/greetings", method = RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> createGreeting(@RequestBody Greeting greeting) {
+		Greeting savedGreeting = save(greeting);
+		return new ResponseEntity<Greeting>(savedGreeting, HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(value="/api/greetings", method = RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> updateGreeting(@RequestBody Greeting greeting) {
+		Greeting updatedGreeting = save(greeting);
+		if(updatedGreeting == null) {
+			return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<Greeting>(updatedGreeting, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/api/greetings/{id}", method=RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> deleteGreeting(@PathVariable("id") BigInteger id, @RequestBody Greeting greeting) {
+		boolean deleted = delete(id);
+		if(!deleted) {
+			return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<Greeting>(HttpStatus.NO_CONTENT);
+	}
+}
+
+
+
+
+
+
+
+
+
