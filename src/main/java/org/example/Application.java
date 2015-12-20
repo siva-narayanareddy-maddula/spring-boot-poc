a37a471e98934d50e866248d6e4832d523312004
Siva Narayana Reddy Maddula <siva_narayanareddy_maddula@apple.com>
2015-12-21 01:25:02 +0530

    Added transaction management

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,12 +2,14 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Spring Boot main Application
  *
  */
 @SpringBootApplication
+@EnableTransactionManagement
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/org/example/ws/service/impl/GreetingServiceImpl.java
+++ src/main/java/org/example/ws/service/impl/GreetingServiceImpl.java
@@ -10,8 +10,11 @@ import org.example.ws.repository.GreetingRepository;
 import org.example.ws.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceImpl implements GreetingService {
 
 	@Autowired
@@ -30,15 +33,22 @@ public class GreetingServiceImpl implements GreetingService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting create(Greeting greeting) {
 		if(greeting.getId() != null) {
 			return null;
 		}
 		Greeting savedGreeting = greetingRepo.save(greeting);
+		
+		// Illustrate Tx rollback
+		if(savedGreeting.getId() == 4L) {
+			throw new RuntimeException("Roll me back");
+		}
 		return savedGreeting;
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting update(Greeting greeting) {
 		Greeting greetingPersisted = findOne(greeting.getId());
 		if(greetingPersisted == null) {
@@ -49,6 +59,7 @@ public class GreetingServiceImpl implements GreetingService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void delete(Long id) {
 		greetingRepo.delete(id);
 	}
